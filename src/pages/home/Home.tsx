454439a9432d96464c630e7fddbb173dd6412584
eliasinacio <0.elias.inacio@gmail.com>
2022-12-22 22:24:01 -0300

    feat: routes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import './styles.sass'
 
-function App() {
+function Home() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="Home">
       
     </div>
   )
 }
 
-export default App
+export default Home
